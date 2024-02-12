1ebce762cf1d3a918a78486ca85eb688b67b0c9c
Sourya Bhattacharya <37332817+sourya1995@users.noreply.github.com>
2024-02-12 10:33:43 +0530

    initial commit

---
+++ discord-clone/app/(main)/(routes)/page.tsx
@@ -0,0 +1,17 @@
+import { Button } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
+import { UserButton } from  "@clerk/nextjs";
+
+const state = true;
+
+export default function Home() {
+  return (
+    <div>
+      <UserButton afterSignOutUrl="/" />
+    </div>
+  );
+}
+
+//comment
+//comment
+//comment
