4b3394e8bcecb3550685cbc124933607e18dbfbe
Sourya Bhattacharya <37332817+sourya1995@users.noreply.github.com>
2024-02-12 10:57:45 +0530

    feature: light/dark mode toggle

--- discord-clone/app/(main)/(routes)/page.tsx
+++ discord-clone/app/(main)/(routes)/page.tsx
@@ -1,3 +1,4 @@
+import { ModeToggle } from "@/components/mode-toggle";
 import { Button } from "@/components/ui/button";
 import { cn } from "@/lib/utils";
 import { UserButton } from  "@clerk/nextjs";
@@ -8,6 +9,7 @@ export default function Home() {
   return (
     <div>
       <UserButton afterSignOutUrl="/" />
+      <ModeToggle />
     </div>
   );
 }
